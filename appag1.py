f9ff8839789070eb4cb1663c028280f0f948347b
Valeria Espinal <valeria.espinal@upb.edu.co>
2022-11-17 20:18:48 -0500

    Version 3

--- appag1.py
+++ appag1.py
@@ -1,32 +1,52 @@
-import flask, render_template, request, xlsxwriter
+import flask, render_template, request, xlsxwriter, openpyxl
 
 app1 = flask.Flask(__main__)
+libro = xlsxwriter.Book('/data/basedatos.xlsx')
+pagina = libro.add_sheet()
+fila = 0
+columna = 0
 
 @app1.route('/') #url principal
 def inicio():
 	return "pagina de inicio de la red social"
+@app1.route ('/inicio_sesion')
+def iniciar_sesion():
+        return render_template("/templates/iniciosesion.html")
 
 @app1.route ('/iniciar_sesion')
 def iniciar():
-	return "iniciar sesión"
+        correo = request.form['correo']
+        clave = request.form['clave']
+        ps = openpyxl.load_workbook('basedatos.xlsx')
+        pagina1 = ps['Sheet1']
+        correo_existe = False
+        for columna in range(1, pagina1.max_row + 1):
+                correo1 = pagina1['B' + str(columna)].value
+                if(correo1==correo):
+                        correo_existe = True
+                        if(pagina1['C' + str(columna)].value=clave):
+                                return ("Sesión correctamente iniciada")
+                        else return("La clavae es incorrecta.")
+
+        if(correo_existe = False):
+                return("El correo no existe.")
 
 @app1.route('/registro')
 def registrar:
-	return render_template("form.html")
-@app1.route ('/crear_usuario')
+	return render_template("registro.html")
+@app1.route ('/crear_usuario',methods=['POST'])
 def crear():
 	nombre_comp = request.form['nombre_comp']
 	correo = request.form['correo']
 	clave = request.form['clave']
-	libro = xlsxwriter.Book('/data/basedatos.xlsx')
-	pagina = libro.add_sheet()
-	fila = 0
-	columna = 0
+	
 	contenido = [nombre, correo, clave]
 	
 	for elemento in contenido:
 		pagina.write(fila, columna, elemento)
 		columna += 1
+
+	fila += 1
 	
 	
 return "Su nombre es " + nombre_comp + ", su correo es " + correo + ", inicie sesión con su correo y contraseña "
@@ -39,10 +59,11 @@ def crear_perfil():
 
 @app1.route ('/cerrar_sesion')
 def cerrar_sesion():
-	return "cerrando sesión del usuario "
+	return render_template('/templates/cerrar_sesion.html')
 
 @app1.route ('/irperfil')
 def navegar_perfil():
+        
 	return "navegando por un perfil publico"
 
 
